6246c57bad0d4d55c6547a0888d8f6bf90edf9a7
ps <599103949@qq.com>
2018-03-06 00:17:53 +0800

    1.1

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@
 //描述：中国古代数学家张丘建在他的《算经》中提出了一个著名的“百钱白鸡问题”：
 //	公鸡值五钱，一只母鸡值三钱，三只小鸡值一钱，
 //	现在要用百钱买百鸡，请问公鸡、母鸡、小鸡各多少只？
+//注意 rooster 和 hen 的最大数
 #include <stdio.h>
-
+#define TOTALNUM 100
+#define TOTALMONEY 100
+#define ROOSTERMONEY 5.0
+#define HENMONEY 3.0
+#define CHICKMONEY 1/3.0
 int main()
 {
+	int roosterNum, henNum;
+
+	for(roosterNum = 0;roosterNum < TOTALNUM/ROOSTERMONEY; roosterNum++)
+		for(henNum = 0;henNum < TOTALNUM/HENMONEY; henNum++)
+			if(roosterNum*ROOSTERMONEY + henNum*HENMONEY + (TOTALNUM - roosterNum - henNum)*CHICKMONEY == TOTALMONEY)
+				printf("roosterNUM = %d\nhenNum = %d\nchickNum = %d\n\n",roosterNum, henNum, TOTALNUM - roosterNum - henNum);
 	return 0;
 }
